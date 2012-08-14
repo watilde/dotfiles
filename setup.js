48c7d6c4bbd69fe84c30fedcc7577ee3f8d62453
Daijiro Wachi <daijiro.wachi@gmail.com>
2012-08-15 08:09:32 +0900

    modify setup.js

--- setup.js
+++ setup.js
@@ -1,24 +1,29 @@
-var grepList = ['"^\.$"','"^\.\.$"', '.git', 'setup.js'];
-
-var shellCommand = function(hyphenE){
-	var proto = 'ls -a ./ | grep -v';
-	for(var i=0; i<hyphenE.length; i++){
-		proto += ' -e '+hyphenE[i];
-	}
-	return proto;
-};
-
-function custArray(stdout) {
-	stdout = stdout.split('\n');
-	stdout = stdout.splice(0, stdout.length-1);
-	return stdout;
-}
-
 var exec = require('child_process').exec;
 
-exec(shellCommand(grepList), function(err, stdout, stderr){
-	if(!err){
-		stdout = custArray(stdout);
-		console.log(stdout);
+var dotfiles = function dotfiles(){
+	var shellCommand = function shellCommand(hyphenE){
+		var proto = 'ls -a ./ | grep -v';
+		for(var i=0; i<hyphenE.length; i++){
+			proto += ' -e '+hyphenE[i];
+		}
+		return proto;
+	};
+
+	var custArray = function custArray(stdout) {
+		stdout = stdout.split('\n');
+		stdout = stdout.splice(0, stdout.length-1);
+		return stdout;
 	}
-});
+
+	var grepList = ['"^\.$"','"^\.\.$"', '.git', 'setup.js'];
+	exec(shellCommand(grepList), function(err, stdout, stderr){
+		if(!err && !stderr){
+			stdout = custArray(stdout);
+			console.log(stdout);
+			return (stdout);
+		}
+	});
+};
+
+var a = dotfiles();
+console.log(a);
