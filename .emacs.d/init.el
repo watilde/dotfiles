808aaef7aad4d39a188b9e1221ebcfa968cfe56c
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-03-12 00:53:04 +0900

    Hello emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,4 @@
+(xterm-mouse-mode t)
+(mouse-wheel-mode t)
+(global-set-key   [mouse-4] '(lambda () (interactive) (scroll-down 1)))
+(global-set-key   [mouse-5] '(lambda () (interactive) (scroll-up   1)))
