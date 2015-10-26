84293bfd1ecf9f0a1c9981174a871582564ba13b
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-10-26 12:18:22 +0100

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,12 @@
+;; Access Marmalade and MELPA repositories, in addition to GNU ELPA
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
+;; js2-mode
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
 (xterm-mouse-mode t)
 (mouse-wheel-mode t)
 (global-set-key   [mouse-4] '(lambda () (interactive) (scroll-down 1)))
