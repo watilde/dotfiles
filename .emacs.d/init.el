a4b11423f1f3e181963719af6388ee360a9488d3
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-11-04 23:58:36 +0100

    Update init.el to add minor options

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,5 @@
 ;; list the packages
-(setq package-list '(js2-mode))
+(setq package-list '(js2-mode tabbar))
 
 ;; Access Marmalade and MELPA repositories, in addition to GNU ELPA
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -21,8 +21,35 @@
 ;; js2-mode
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
+;; tabbar
+(tabbar-mode 1)
+(tabbar-mwheel-mode -1)
+(setq tabbar-buffer-groups-function nil)
+(setq tabbar-use-images nil)
+(setq tabbar-auto-scroll-flag nil)
+(dolist (btn '(tabbar-buffer-home-button
+               tabbar-scroll-left-button
+               tabbar-scroll-right-button))
+  (set btn (cons (cons "" nil)
+                 (cons "" nil))))
+(global-set-key (kbd "C-]") 'tabbar-forward-tab)
+(global-set-key (kbd "C-[") 'tabbar-backward-tab)
+
 ;; display line number
 (global-linum-mode t)
+
+;; no message
+(setq inhibit-startup-message t)
+
+;; no backup
+(setq backup-inhibited t)
+
+;; scroll line by line
+(setq scroll-step 1)
+
+;; no line wrap
+(setq-default truncate-lines t)
+(setq truncate-partial-width-windows t)
 
 ;; enable mouse
 (xterm-mouse-mode t)
