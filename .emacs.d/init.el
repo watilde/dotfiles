0cb0eaf9aeb15b09b2178a80c880985e503b195f
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-11-05 00:38:27 +0100

    Add tabbar setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,6 @@
+
+
+
 ;; list the packages
 (setq package-list '(js2-mode tabbar))
 
@@ -34,6 +37,35 @@
                  (cons "" nil))))
 (global-set-key (kbd "C-]") 'tabbar-forward-tab)
 (global-set-key (kbd "C-[") 'tabbar-backward-tab)
+
+(set-face-attribute 'tabbar-default nil
+        :background "white"
+        :foreground "black"
+        :family "Monaco")
+
+(set-face-attribute 'tabbar-selected nil
+        :background "black"
+        :foreground "white"
+        :box '(:line-width 1 :color "black" :style nil))
+
+(set-face-attribute 'tabbar-unselected nil
+        :box '(:line-width 1 :color "gray20" :style nil))
+
+(set-face-attribute 'tabbar-highlight nil
+        :underline nil
+        :background "goldenrod")
+
+(set-face-attribute 'tabbar-button nil
+        :foreground "white"
+        :box '(:line-width 1 :color "gray50" :style nil))
+
+(set-face-attribute 'tabbar-separator nil
+        :background "white"
+        :foreground "grey50"
+        :height 1.0)
+
+(setq tabbar-separator '(1))
+
 
 ;; display line number
 (global-linum-mode t)
