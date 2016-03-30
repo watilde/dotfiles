68f0ca859701e5eb33d803d7a5cb8dbfdd32d5c0
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-03-30 12:21:27 +0200

    minor tweaks to indent

--- .emacs.d/package.el
+++ .emacs.d/package.el
@@ -2,12 +2,12 @@
 (setq package-list '(js2-mode tabbar init-loader))
 
 ;; Access Marmalade and MELPA repositories, in addition to GNU ELPA
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
+(setq package-archives
+  '( ("gnu" . "http://elpa.gnu.org/packages/")
+     ("marmalade" . "https://marmalade-repo.org/packages/")
+     ("melpa" . "http://melpa.org/packages/")))
 
 (setq package-user-dir (locate-user-emacs-file "packages"))
-
 
 ;; activate all the packages (in particular autoloads)
 (package-initialize)
