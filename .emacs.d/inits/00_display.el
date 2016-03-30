cd7aedad3c1cfa40408945acb5e3fff8892bc3dd
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-03-30 11:48:13 +0200

    Update emacs.d to separate files

---
+++ .emacs.d/inits/00_display.el
@@ -0,0 +1,21 @@
+;; display line number
+(global-linum-mode t)
+
+;; no message
+(setq inhibit-startup-message t)
+
+;; no backup
+(setq backup-inhibited t)
+
+;; scroll line by line
+(setq scroll-step 1)
+
+;; no line wrap
+(setq-default truncate-lines t)
+(setq truncate-partial-width-windows t)
+
+;; enable mouse
+(xterm-mouse-mode t)
+(mouse-wheel-mode t)
+(global-set-key   [mouse-4] '(lambda () (interactive) (scroll-down 1)))
+(global-set-key   [mouse-5] '(lambda () (interactive) (scroll-up   1)))
---
+++ .emacs.d/package.el
@@ -0,0 +1,22 @@
+;; list the packages
+(setq package-list '(js2-mode tabbar init-loader))
+
+;; Access Marmalade and MELPA repositories, in addition to GNU ELPA
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+
+(setq package-user-dir (locate-user-emacs-file "packages"))
+
+
+;; activate all the packages (in particular autoloads)
+(package-initialize)
+
+;; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
