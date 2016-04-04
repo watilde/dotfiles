d54f58a5f4e5cdfbb0a8e3a8f33cfd5c2045549b
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-04-04 11:35:24 +0200

    emacs: move neotree to right side

--- .emacs.d/inits/30_neotree.el
+++ .emacs.d/inits/30_neotree.el
@@ -1,6 +1,11 @@
 (require 'neotree)
+
 (global-set-key (kbd "C-c t") 'neotree-toggle)
+
 (setq neo-smart-open t)
+
+(setq neo-window-position "right")
+
 (custom-set-faces
  '(col-highlight ((t (:background "color-233"))))
  '(hl-line ((t (:background "color-233"))))
