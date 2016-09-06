9489da1377705628ff1ee6c62a4b4f13f193d7db
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-09-06 19:47:00 +0200

    bash: fix the path of bash-it

--- .bash/inits/00_bash_it.sh
+++ .bash/inits/00_bash_it.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to the bash it configuration
-export BASH_IT=$HOME/.bash_it
+export BASH_IT=$HOME/.bash/packages/bash-it
 
 # Lock and Load a custom theme file
 # location /.bash_it/themes/
