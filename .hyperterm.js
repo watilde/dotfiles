2393edad9d408b936b601cfdb3987157da3fadf9
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-09-19 15:37:52 +0200

    hyperterm: add hyperborder

--- .hyperterm.js
+++ .hyperterm.js
@@ -55,8 +55,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-  ],
+  plugins: ["hyperborder"],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
