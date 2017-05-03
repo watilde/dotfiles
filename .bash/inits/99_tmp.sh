a39eb38ce5ebfa9b8e701101167f7ecd49a05659
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-05-03 15:03:38 +0200

    bash: update x64-test-all command

--- .bash/inits/99_tmp.sh
+++ .bash/inits/99_tmp.sh
@@ -3,4 +3,4 @@
 # V8
 alias x64-build="ninja -C out/x64.release"
 alias x64-test="out/x64.release/d8 test/webkit/resources/standalone-pre.js test/webkit/fast/js/JSON-parse-reviver.js"
-alias x64-test-all="tools/run-tests.py --arch=x64"
+alias x64-test-all="tools/run-tests.py --arch-and-mode=x64.release"
