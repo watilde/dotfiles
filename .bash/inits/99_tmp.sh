3ec0007f9e874f181293759c5cfcb86adbba773b
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-05-03 14:50:08 +0200

    bash: fix typo in aliases

--- .bash/inits/99_tmp.sh
+++ .bash/inits/99_tmp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # V8
-alias x64-build="ninja -C out.gn/x64.release"
+alias x64-build="ninja -C out/x64.release"
 alias x64-test="out/x64.release/d8 test/webkit/resources/standalone-pre.js test/webkit/fast/js/JSON-parse-reviver.js"
 alias x64-test-all="tools/run-tests.py --arch=x64"
