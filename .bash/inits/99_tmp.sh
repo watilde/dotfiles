cce20b436ca6cc437adc72962d8737ae845bf03a
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-05-05 08:08:48 +0200

    bash: update v8-build

--- .bash/inits/99_tmp.sh
+++ .bash/inits/99_tmp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # V8
-alias v8-build="ninja -C out.gn/x64.release"
+alias v8-build="ninja -C out/x64.release"
 alias v8-test="out.gn/x64.release/d8 test/webkit/resources/standalone-pre.js test/webkit/fast/js/JSON-parse-reviver.js"
 alias v8-test-all="tools/run-tests.py --arch-and-mode=x64.release"
 alias v8-issues="open https://bugs.chromium.org/p/v8/issues/list"
