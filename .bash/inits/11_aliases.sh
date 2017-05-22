85c465a36a21c5bce9f75b9b47e811035eba48be
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-05-22 12:57:13 +0200

    bash: use GNU emacs instead of emacs.app

--- .bash/inits/11_aliases.sh
+++ .bash/inits/11_aliases.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Emacs
-if [ -e '/Applications/Emacs.app' ]; then
-  alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs -nw'
-  alias emacs.app='/Applications/Emacs.app/Contents/MacOS/Emacs'
-fi
-
-# Vim
-if [ -e '/Applications/MacVim.app' ]; then
-  alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
-  alias vim.app='/Applications/MacVim.app/Contents/MacOS/MacVim'
-fi
-
 # List
 case "${OSTYPE}" in
 darwin*)
@@ -35,11 +23,6 @@ fi
 # Chest
 if [ -e "$__CHEST/node_modules/.bin/chest" ]; then
   alias chest="$__CHEST/node_modules/.bin/chest"
-fi
-
-# Yarn
-if [ -e "$__CHEST/node_modules/.bin/yarn" ]; then
-  alias yarn="$__CHEST/node_modules/.bin/yarn"
 fi
 
 # Emoji
