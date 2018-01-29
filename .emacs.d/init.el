636c84b210331dae0e7b9c9ae0a8616ce309e886
Daijiro Wachi <daijiro.wachi@gmail.com>
2018-01-29 15:24:37 +0100

    emacs: update pkgs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,7 +21,6 @@
     (tabbar swift-mode sublime-themes sass-mode rust-mode rainbow-delimiters python-mode neotree js2-mode init-loader haskell-mode)))
  '(tabbar-separator (quote (0.5))))
 (custom-set-faces
-
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
--- .emacs.d/package.el
+++ .emacs.d/package.el
@@ -1,24 +1,25 @@
 ;; list the packages
 (setq package-list '(
-  init-loader
-  tabbar
-  neotree
-  sublime-themes
-  js2-mode
-  haskell-mode
-  swift-mode
-  rust-mode
   cc-mode
+  flycheck
+  haskell-mode
+  init-loader
+  js2-mode
+  magit
+  neotree
   python-mode
+  rainbow-delimiters
+  rust-mode
   sass-mode
-  rainbow-delimiters))
+  swift-mode
+  tabbar))
 
 ;; Access Marmalade and MELPA repositories, in addition to GNU ELPA
 (setq package-archives
   '( ("gnu" . "https://elpa.gnu.org/packages/")
      ("marmalade" . "https://marmalade-repo.org/packages/")
-     ("melpa" . "https://melpa.org/packages/")
-     ("melpa-stable" . "https://stable.melpa.org/packages/")))
+     ("melpa-stable" . "https://stable.melpa.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")))
 
 (setq package-user-dir (locate-user-emacs-file "packages"))
 
