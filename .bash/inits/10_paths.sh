5984a7e1e63271c6f81fc5d7af8bf3dd93266942
Daijiro Wachi <daijiro.wachi@gmail.com>
2020-09-22 10:21:50 +0900

    bash: add nvm support

--- .bash/inits/10_paths.sh
+++ .bash/inits/10_paths.sh
@@ -15,6 +15,8 @@ fi
 if [ -e "$HOME/.nvm" ]; then
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 fi
 
 # Ruby
